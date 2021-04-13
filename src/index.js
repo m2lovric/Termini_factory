7835725e69a3eb22021330f463ede3d781c908e3
Matteo <m2lovric@gmail.com>
2021-04-13 11:55:10 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const addContent = () => {
-  console.log("addContent");
   const year = document.getElementsByClassName("year");
   const month = document.getElementsByClassName("month");
   const day = document.getElementsByClassName("day");
@@ -9,66 +8,78 @@ const addContent = () => {
   const monthsArr = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
   for (let i = 2022; i > 1900; i--) {
-    let option = document.createElement("option");
-    let text = document.createTextNode(`${i}`);
+    const option = document.createElement("option");
+    const text = document.createTextNode(`${i}`);
     option.appendChild(text);
     option.setAttribute("value", `${year}`);
 
-    year.appendChild(option);  
+    for (let item of year) {
+      item.appendChild(option)
+    }
   }
 
   for (let i = 0; i < 12; i++) {
-    let option = document.createElement("option");
-    let text = document.createTextNode(`${monthsArr[i]}`);
+    const option = document.createElement("option");
+    const text = document.createTextNode(`${monthsArr[i]}`);
     option.appendChild(text);
     option.setAttribute("value", `${monthsArr[i]}`);
 
-    month.appendChild(option);  
+    for (let item of month) {
+      item.appendChild(option)
+    }
   }
 
   for (let i = 1; i < 32; i++) {
-    let option = document.createElement("option");
-    let text = document.createTextNode(`${i}`);
+    const option = document.createElement("option");
+    const text = document.createTextNode(`${i}`);
     option.appendChild(text);
     option.setAttribute("value", `${i}`);
 
-    day.appendChild(option);  
-  }
-
-  for (let i = 0; i < 24; i++) {
-    for (let j = 0; j < 4; j+=3) {
-      let option = document.createElement("option");
-      let text = document.createTextNode(`${i}:${j}0`);
-      option.appendChild(text);
-      option.setAttribute("value", `${i}`);
-
-      start.appendChild(option);   
+    for (let item of day) {
+      item.appendChild(option)
     }
   }
 
   for (let i = 0; i < 24; i++) {
     for (let j = 0; j < 4; j+=3) {
-      let option = document.createElement("option");
-      let text = document.createTextNode(`${i}:${j}0`);
+      const option = document.createElement("option");
+      const text = document.createTextNode(`${i}:${j}0`);
       option.appendChild(text);
       option.setAttribute("value", `${i}`);
 
-      end.appendChild(option);   
+      for (let item of start) {
+        item.appendChild(option)
+      }
+    }
+  }
+
+  for (let i = 0; i < 24; i++) {
+    for (let j = 0; j < 4; j+=3) {
+      const option = document.createElement("option");
+      const text = document.createTextNode(`${i}:${j}0`);
+      option.appendChild(text);
+      option.setAttribute("value", `${i}`);
+
+      for (let item of end) {
+        item.appendChild(option)
+      }
     }
   }
 }
 
 const termComponent = () => {
-  const body = document.querySelector('body');
+  const body = document.getElementById('main');
   const article = document.createElement("article");
   const classesArr = ["year", "month", "day", "start", "end"];
 
-  classesArr.forEach(el => {
-    const select = document.createElement("select");
-    select.setAttribute("class", el);
+  setTimeout(() => {
+    classesArr.forEach(el => {
+      const select = document.createElement("select");
+      select.className = el;
 
-    article.appendChild(select);
-  });
+      article.appendChild(select);
+    });
+  })
   
   body.appendChild(article);
   addContent();
@@ -76,7 +87,7 @@ const termComponent = () => {
 
 termComponent();
 
-const body = document.querySelector('body');
+const body = document.getElementById('main');
 
 let addBtn = document.createElement("button");
 let text = document.createTextNode('Dodaj još jedan termin');
@@ -84,7 +95,6 @@ addBtn.setAttribute("id", "btn");
 addBtn.appendChild(text);
 body.appendChild(addBtn);   
 
-
 document.querySelector("#btn").addEventListener('click', () => {
-  console.log('click');
+  termComponent();
 })
